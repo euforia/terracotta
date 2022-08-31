b42c49b861a563673e5fdc29440614d8fbe70c80
euforia <euforia>
2022-08-31 13:28:21 -0700

    Initial commit

---
+++ lambda-api/main.tf
@@ -0,0 +1,87 @@
+data "aws_apigatewayv2_api" "apigw" {
+  api_id = var.api_gateway_id
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename         = var.lambda_filepath
+  function_name    = var.lambda_name
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = var.lambda_runtime
+  source_code_hash = filebase64sha256(var.lambda_filepath)
+  handler          = "main"
+  architectures    = [var.lambda_arch]
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.lambda_name}_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow"
+        Sid    = "${var.lambda_name}LambdaAssumeRole"
+      }
+    ]
+  })
+}
+
+// Cloudwatch logging
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "${var.lambda_name}_lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+        Effect   = "Allow"
+      }
+    ]
+  })
+}
+
+// Attach policy
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+
+// Allow api gateway to invoke it.
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${data.aws_apigatewayv2_api.apigw.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id                 = var.api_gateway_id
+  integration_type       = "AWS_PROXY"
+  connection_type        = "INTERNET"
+  payload_format_version = "2.0"
+  integration_uri        = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "apigateway_route" {
+  api_id    = var.api_gateway_id
+  route_key = var.endpoint
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
---
+++ lambda-api/variables.tf
@@ -0,0 +1,34 @@
+
+variable "lambda_filepath" {
+  # default = "../dist/function.zip"
+  description = "Path to lambda zip file"
+}
+
+variable "endpoint" {
+  # default = "POST /deploy"
+  description = "<METHOD> <RESOURCE> the lambda maps to"
+}
+
+variable "lambda_handler_name" {
+  default     = "main"
+  description = "Entrypoint handler name"
+}
+
+variable "lambda_runtime" {
+  default     = "go1.x"
+  description = "Lambda runtime to use"
+}
+
+variable "lambda_arch" {
+  default     = "x86_64"
+  description = "Lambda code architecture [ 'x86_64' | 'arm64' ]"
+}
+
+variable "lambda_name" {
+  # default = "deploy"
+  description = "Name to give the lambda"
+}
+
+variable "api_gateway_id" {
+  default = "r4k51ldkjh"
+}
